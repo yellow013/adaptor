071ded02ef6023e75c52f57db16ef0b9842a9370
yellow013 <wk9988@gmail.com>
2019-05-23 23:04:27 +0800

    update code impl

--- src/main/java/io/ffreedom/jctp/base/JctpRspValidator.java
+++ src/main/java/io/ffreedom/jctp/base/JctpRspValidator.java
@@ -1,12 +1,14 @@
 package io.ffreedom.jctp.base;
 
 import ctp.thostapi.CThostFtdcRspInfoField;
+import io.ffreedom.common.utils.StringUtil;
 
 public final class JctpRspValidator {
 
 	public static final void validateRspInfo(String spiMethodName, CThostFtdcRspInfoField RspInfo) {
 		if (RspInfo != null && RspInfo.getErrorID() != 0)
-			throw new JctpRspException(spiMethodName, RspInfo.getErrorID(), RspInfo.getErrorMsg());
+			throw new JctpRspException(spiMethodName, RspInfo.getErrorID(),
+					StringUtil.gbkConversionToUtf8(RspInfo.getErrorMsg()));
 	}
 
 }
