a0bbbeedd8e8b7593ba9f2f02f2cbdfae2a632bd
yellow013 <wk9988@gmail.com>
2021-10-11 23:29:24 +0800

    update adaptor ib

---
+++ adaptor-ib/src/main/java/io/horizon/market/instrument/impl/JapanFutures.java
@@ -0,0 +1,10 @@
+package io.horizon.market.instrument.impl;
+
+public final class JapanFutures extends AbstractInstrument {
+
+	public JapanFutures(JapanFuturesSymbol symbol, int term) {
+		// TODO 设置id和code
+		super(0, "", symbol);
+	}
+
+}
