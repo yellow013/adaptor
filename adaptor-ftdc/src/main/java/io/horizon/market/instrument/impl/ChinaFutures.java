c96bd253ff829da2cf9de1e4b373c524ff165b9c
yellow013 <wk9988@gmail.com>
2021-10-11 23:28:07 +0800

    update instrument impl

---
+++ adaptor-ftdc/src/main/java/io/horizon/market/instrument/impl/ChinaFutures.java
@@ -0,0 +1,37 @@
+package io.horizon.market.instrument.impl;
+
+public final class ChinaFutures extends AbstractInstrument {
+
+	private final PriorityCloseType priorityCloseType;
+
+	/**
+	 * 
+	 * @param symbol
+	 * @param term
+	 */
+	public ChinaFutures(ChinaFuturesSymbol symbol, int term) {
+		this(symbol, term, String.valueOf(term));
+	}
+
+	/**
+	 * 
+	 * @param symbol
+	 * @param term
+	 * @param codeTail
+	 */
+	public ChinaFutures(ChinaFuturesSymbol symbol, int term, String codeTail) {
+		super(symbol.acquireInstrumentId(term), symbol.getSymbolCode() + codeTail, symbol);
+		this.priorityCloseType = symbol.getPriorityCloseType();
+	}
+
+	@Override
+	public PriorityCloseType getPriorityCloseType() {
+		return priorityCloseType;
+	}
+
+	@Override
+	public boolean isAvailableImmediately() {
+		return true;
+	}
+
+}
